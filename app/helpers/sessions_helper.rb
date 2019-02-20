2e1cac57722eba9aa76154d2dc0c2e944fe91402
Yuki Ueno <yuki.weaver1204@gmail.com>
2019-02-20 14:24:16 +0000

    新勤怠6章1

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,20 @@
+module SessionsHelper
+  
+  # 引数に渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # 現在ログイン中のユーザーを返す (ただし、いる場合のみ)
+  def current_user  
+    if session[:user_id]
+    #   if @current_user.nil?  と下記同義
+    #      @current_user = User.find_by(id: session[:user_id])
+    #   else
+    #       @current_user
+    #   end
+    # @current_user = @current_user || User.find_by(id: session[:user_id]) と下記同義
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get  '/signup', to: 'users#new'
+
+  root   'static_pages#home'
+  get    '/signup', to: 'users#new'
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   resources :users
 end
