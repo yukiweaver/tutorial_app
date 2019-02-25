c04becd99bd985c1642fe98a86224a5605a086bc
Yuki Ueno <yuki.weaver1204@gmail.com>
2019-02-25 13:44:37 +0000

    Finish basic information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy]
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
   
   def index
     @users = User.paginate(page: params[:page])
@@ -47,13 +47,32 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "削除しました。"
     redirect_to users_url
-    
+  end
+  
+  # 基本情報編集ページ
+  def edit_basic_info
+    @user = User.find(params[:id])
+  end
+  
+  # 基本情報更新
+  def update_basic_info
+    @user = User.find(params[:id])
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "基本情報を更新しました。"
+      redirect_to @user
+    else
+      render 'edit_basic_info'
+    end
   end
   
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
+    
+    def basic_info_params
+      params.require(:user).permit(:basic_time, :work_time)
     end
     
     # beforeアクション
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  # 基本時間などの値を、指定の書式にフォーマットして返す
+  def format_basic_time(datetime)
+    format("%.2f", ((datetime.hour * 60) + datetime.min) /60.0)
+  end
 end
